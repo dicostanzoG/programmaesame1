cf2332df52c6a58875924063def40999175b56bf
dicostanzoG <giulia.dicostanzo@stud.unifi.it>
2016-12-06 21:58:42 +0100

    Add files via upload

--- Block.h
+++ Block.h
@@ -1,36 +1,33 @@
-//
-// Created by noble on 29/11/2016.
-//
-
 #ifndef PROGRAMMAESAME_BLOCK_H
 #define PROGRAMMAESAME_BLOCK_H
 
 #include "Item.h"
-#include <SFML/Graphics.hpp>
 
 class Block {
 
 public:
+    Block (float x=0, float y=0, bool destr=false, bool empty=false):
+                 x_block(x), y_block(y),  destroyed(destr), empty(empty) {}
+    ~Block(){}
+
     bool DropItem(bool empty){
-        Block :: empty = empty; // empty falso
+        Block::empty = empty;
     }
 
     void setPos_X(float x_block){
-        Block :: x_block = x_block;
+        Block::x_block = x_block;
     }
 
     float getPos_X(){
         return x_block;
-
     }
 
-    void setPos_Y(float yb){
-        Block :: y_block = y_block;
+    void setPos_Y(float y_block){
+        Block::y_block = y_block;
     }
 
     float getPos_Y(){
         return y_block;
-
     }
 
 private:
